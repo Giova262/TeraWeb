113f5bfbb3f01c237fb91f82196f168ed4892d92
Giova262 <masterpat45@gmail.com>
2020-01-10 23:55:55 -0300

    Updates

---
+++ precache-manifest.ed668f383d2c5c9240cb555fa4f42078.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ae222d72bc3a9d957b89e9d66def08f",
+    "url": "/TeraWeb/index.html"
+  },
+  {
+    "revision": "2445f6c0f6f415b297d5",
+    "url": "/TeraWeb/static/css/main.df0c1e27.chunk.css"
+  },
+  {
+    "revision": "eb59bbc6b8dd69545f13",
+    "url": "/TeraWeb/static/js/2.0862e3f1.chunk.js"
+  },
+  {
+    "revision": "133cd7ddb6743077c81a5b83934783d1",
+    "url": "/TeraWeb/static/js/2.0862e3f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2445f6c0f6f415b297d5",
+    "url": "/TeraWeb/static/js/main.1a0e18f4.chunk.js"
+  },
+  {
+    "revision": "7c3094b8764249ef11c8",
+    "url": "/TeraWeb/static/js/runtime-main.c67c5526.js"
+  },
+  {
+    "revision": "0610a2ee260a57fb0e077e04ad771df9",
+    "url": "/TeraWeb/static/media/asd.0610a2ee.svg"
+  },
+  {
+    "revision": "c072fdb193ac1c340ce3825f958b1aa8",
+    "url": "/TeraWeb/static/media/estrellas.c072fdb1.svg"
+  },
+  {
+    "revision": "3fb2298ce68fcc7066b18e45c37419e6",
+    "url": "/TeraWeb/static/media/fondo.3fb2298c.jpg"
+  },
+  {
+    "revision": "5622bedc9cbfa81e24ea4fb4c9e71145",
+    "url": "/TeraWeb/static/media/paper.5622bedc.jpg"
+  },
+  {
+    "revision": "763b4716501cc74b2972101f15c83cee",
+    "url": "/TeraWeb/static/media/userDefect.763b4716.png"
+  }
+]);
